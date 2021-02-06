4ca004bfa3df65ef8533d075a7732ff46c6534db
Atila OLGUNCAN <atila.olguncan@gmail.com>
2021-02-06 22:04:53 +0300

    6 Şubat Mola 1

---
+++ Core/DataAccess/EntiityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntiityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+    {
+    }
+}
