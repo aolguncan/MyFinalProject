b3f899f71b3364bb6db532060b677f51967f1582
Atila OLGUNCAN <atila.olguncan@gmail.com>
2021-02-11 00:15:34 +0300

    10 Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleUI
     //SOLID
     //Open Closed Principle
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
             //Data Transformation Object
@@ -30,10 +30,21 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
         }
     }
 }
